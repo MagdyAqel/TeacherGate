4f3588929a574e8b65ba5a7675f002288f0fe5cf
MagdyAqel <111590180+MagdyAqel@users.noreply.github.com>
2026-06-26 16:56:11 +0300

    إضافة إعدادات Firebase للبوابة

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCIOECUiEvL9HDqjW0X7U3ekW5qeGxIEUk",
+  authDomain: "fir-test-e7a14.firebaseapp.com",
+  databaseURL: "https://fir-test-e7a14-default-rtdb.firebaseio.com",
+  projectId: "fir-test-e7a14",
+  storageBucket: "fir-test-e7a14.firebasestorage.app",
+  messagingSenderId: "828970446402",
+  appId: "1:828970446402:web:68dd31917301193ebc47db"
+};
